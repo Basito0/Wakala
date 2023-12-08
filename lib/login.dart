3cff93059f76f8dd0cd4432b29c47264a2087521
Bastián <bas_bece@hotmail.com>
2023-12-08 18:26:05 -0300

    modificaciones en login y la dependencia sql

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,8 +8,31 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: const Center(
-      child: Text('Hello World!'),
+        body: Column(
+      children: [
+        Center(
+          child: TextFormField(
+            decoration: const InputDecoration(
+                constraints: BoxConstraints(maxHeight: 100, maxWidth: 300),
+                labelText: "usuario",
+                hintText: "Ingresa tu correo"),
+          ),
+        ),
+        Center(
+          child: TextFormField(
+            decoration: const InputDecoration(
+                constraints: BoxConstraints(maxHeight: 100, maxWidth: 300),
+                labelText: "contraseña",
+                hintText: "Ingresa tu contraseña"),
+          ),
+        ),
+        FloatingActionButton.extended(
+            onPressed: onPressed, label: Text("Ingresar"))
+      ],
     ));
   }
+}
+
+onPressed() {
+  return 1;
 }
