f157a3a4fe4477363139703ae38496a306839735
orbb4 <114522263+orbb4@users.noreply.github.com>
2023-12-09 19:32:44 -0300

    Pantalla de posts obtenidos desde api, botones no hacen nada

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,96 @@
-import 'package:evaluacion2/wakalaList.dart';
 import 'package:flutter/material.dart';
-
-import 'login.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Wakala',
+      home: PostsScreen(),
+    );
+  }
+}
+
+class PostsScreen extends StatefulWidget {
+  @override
+  _PostsScreenState createState() => _PostsScreenState();
+}
+
+class _PostsScreenState extends State<PostsScreen> {
+  Future<List<dynamic>> fetchPosts() async {
+    try {
+      var url = Uri.https("396b-138-84-33-71.ngrok-free.app", "/posts");
+      final response = await http.get(url, headers: {'ngrok-skip-browser-warning': 'true'});
+      print(response.body);
+      if (response.statusCode == 200) {
+        
+        return json.decode(response.body);
+      } else {
+        print('Request failed with status: ${response.statusCode}');
+        return [];
+      }
+    } catch (e) {
+      
+      print('Error fetching data: $e');
+      return [];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listado de Wakalas"),
       ),
-      home: WakalaList(),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchPosts(),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(child: Text(snapshot.data![index]['sector'] + "\n@" + snapshot.data![index]['username'])),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            side: BorderSide(color: Colors.black),
+                          )
+                        ),
+                        child: Icon(Icons.arrow_forward),
+                      ),
+                    ],
+                   ),          
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(10)
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("Error fetching data"),
+            );
+          }
+          // By default, show a loading spinner
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
