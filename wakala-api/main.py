b0a2cc570bec541e566073ccf5f2f3481bae505e
orbb4 <114522263+orbb4@users.noreply.github.com>
2023-12-15 23:39:22 -0300

    posts soportan comentarios, contadores e imagenes

--- wakala-api/main.py
+++ wakala-api/main.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify, request
 from posts import posts
 from users import users
+from post_images import post_images
+
 app = Flask(__name__)
 
 
-@app.route('/ping')
+@app.route('/ping', methods=["GET"])
 def ping():
     return jsonify({"message": "pong"})
+
+
+@app.route('/posts/<int:post_id>', methods=["GET"])
+def get_post(post_id):
+    for post in posts:
+        if post['id'] == post_id:
+            return jsonify(post)
+    return "no se encontro post"
 
 
 @app.route('/posts')
@@ -14,6 +24,9 @@ def get_posts():
     return jsonify(posts)
 
 
+@app.route('/posts', methods=['POST'])
+# devuelve todos los posts que pertenecen a un usuario dado
+# usado para testeo
 @app.route("/posts/<string:username>")
 def getPostsByUser(username):
     posts_found = []
@@ -27,13 +40,37 @@ def getPostsByUser(username):
 
 @app.route('/posts', methods=["POST"])
 def add_post():
-    print(request.json)
+    # username defaults to anonymous if there is no username given
+    username = request.json.get("username", "anonymous")
+    new_post = {"sector": request.json["sector"],
+                "descripcion": request.json["descripcion"],
+                "username": username,
+                "comentarios": [],
+                "sigue_ahi_count": 0,
+                "ya_no_esta_count": 0,
+                "id": len(posts)
+                }
+    posts.append(new_post)
     return "recibido"
 
 
-@app.route('/userlist')
-def get_users():
-    return jsonify(users)
+@app.route('/userlist/<int:post_id>/images', methods=["POST"])
+def add_images(post_id):
+    request_data = request.get_json()
+    base64image1 = request_data['image1']
+    base64image2 = request_data['image2']
+    new_image = {"post_id": post_id, "base64image1": base64image1, "base64image2": base64image2}
+    post_images.append(new_image)
+    return "recibido"
+
+
+@app.route('/userlist/<int:post_id>/images', methods=["GET"])
+def get_images(post_id):
+    for image in post_images:
+        if image["post_id"] == post_id:
+            return image
+    return "no se encontro imagen"
+
 
 @app.route('/userlist', methods=["POST"])
 def add_user():
@@ -49,6 +86,44 @@ def add_user():
     users.append(new_user)
     return "recibido"
 
+
+# sirve para añadir comentarios o editar los contadores (por ej: sigue ahi) de un post
+@app.route('/posts/<int:post_id>', methods=["PUT"])
+def edit_post(post_id):
+    if "comentario" in request.json:
+        new_comment = {
+            "comentario": request.json["comentario"],
+            "username": request.json["username"]
+        }
+        for post in posts:
+            if post["id"] == post_id:
+                post["comentarios"].append(new_comment)
+                return "recibido"
+        return "Post no encontrado"
+
+    if "sigue_ahi" in request.json:
+        for post in posts:
+            if post["id"] == post_id:
+                if request.json["username"] in post["sigue_ahi_users"]:
+                    post["sigue_ahi_count"] -= request.json["sigue_ahi"]
+                    post["sigue_ahi_users"].remove(request.json["username"])
+                else:
+                    post["sigue_ahi_count"] += request.json["sigue_ahi"]
+                    post["sigue_ahi_users"].append(request.json["username"])
+
+                if request.json["username"] in post["ya_no_esta_users"]:
+                    post["ya_no_esta_count"] -= request.json["ya_no_esta"]
+                    post["ya_no_esta_users"].remove(request.json["username"])
+                else:
+                    post["ya_no_esta_count"] += request.json["ya_no_esta"]
+                    post["ya_no_esta_users"].append(request.json["username"])
+                return "recibido"
+        return "post no encontrado"
+
+
+@app.route('/userlist')
+def get_users():
+    return jsonify(users)
 
 
 if __name__ == '__main__':
--- wakala-api/posts.py
+++ wakala-api/posts.py
@@ -1,25 +1,28 @@
 posts = [{"sector": "Dentro de la multicancha",
           "descripcion": "blablabla blablablablablabla blablabla blablablabl lablablablablablablabla",
           "username": "chalalo",
+          "id": 0,
+          "sigue_ahi_count": 30,
+          "ya_no_esta_count": 1,
           "comentarios": [
               {"username": "jose", "comentario": "la We... feaaaaaaaaaaa"},
               {"username": "juan", "comentario": "yo pise caca ahiiii argggggggg"},
-              {"username": "orbb", "comentario": "ew"}
-          ]
-          },
-         {"sector": "arco medicina",
-          "descripcion": "olor feo",
-          "username": "chalalo",
-          "comentarios": [
-              {"username": "marco", "comentario": "es cierto yo estuve ahi"},
-              {"username": "polo", "comentario": "??"},
-          ]
+              {"username": "orbb", "comentario": "ew"},
+
+          ],
+          "sigue_ahi_users": [],
+          "ya_no_esta_users": []
           },
          {"sector": "lugar muy real calle 123",
           "descripcion": "nukeen este lugar xfa",
           "username": "pera",
+          "id": 1,
+          "sigue_ahi_count": 9,
+          "ya_no_esta_count": 15,
           "comentarios": [
               {"username": "juan1", "comentario": "fake"}
-          ]
+          ],
+          "sigue_ahi_users": [],
+          "ya_no_esta_users": []
           }
          ]
