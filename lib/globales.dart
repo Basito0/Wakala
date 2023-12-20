cd5c03abe5ff3f2299041e702b000fdf11947b70
Bastián <bas_bece@hotmail.com>
2023-12-20 16:49:55 -0300

    agregado cross files y foto1&2

--- lib/globales.dart
+++ lib/globales.dart
@@ -1,5 +1,8 @@
+import 'package:cross_file/cross_file.dart';
+
 String sector = "";
 String descripcion = "";
 String usuarioActual = "none";
 int current_post_id = 0;
-
+XFile? foto1;
+XFile? foto2;
