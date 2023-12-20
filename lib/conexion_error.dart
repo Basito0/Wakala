c8937dedab2e3a62560d4d2f96e230c0afdd1d99
orbb4 <114522263+orbb4@users.noreply.github.com>
2023-12-20 19:12:07 -0300

    Add files via upload

---
+++ lib/conexion_error.dart
@@ -0,0 +1,46 @@
+import "preferencias.dart";
+import 'package:flutter/material.dart';
+
+
+class ConexionErrorScreen extends StatelessWidget {
+  TextEditingController urlController= TextEditingController();
+
+  ConexionErrorScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Problemas de conexión'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+        const Text("Los problemas de conexión pueden deberse a que la URL que utiliza la API haya cambiado. Si es así, por favor enviar un correo a reiturra2021@udec.cl y responderé a la brevedad con la nueva URL.\n Una vez obtenida, copiar y pegar la nueva URL en el campo de abajo."),
+        const SizedBox(height: 20),
+            TextField(
+              controller: urlController,
+              decoration: const InputDecoration(
+                labelText: 'Nueva URL',
+                border: OutlineInputBorder()
+              ),
+             ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                sharedPrefs.setLink(urlController.text);
+                Navigator.of(context).pop();
+              },
+              style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all<Size>(
+                  const Size(double.infinity, 50), 
+                ),
+              ),
+              child: const Text('Guardar nueva URL'),
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
--- lib/funciones_api.dart
+++ lib/funciones_api.dart
@@ -2,10 +2,11 @@ import 'package:http/http.dart' as http;
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'preferencias.dart';
 List<dynamic>? userList;
 
 Future<void> editarContadores(String link, int postId, Map<String, dynamic> contador) async {
-  var url = Uri.https(link, "/posts/$postId");
+  Uri url = sharedPrefs.getUrl("/posts/$postId");
   final response = await http.put(
     url,
     headers: {'Content-Type': 'application/json'},
@@ -21,11 +22,65 @@ Future<void> editarContadores(String link, int postId, Map<String, dynamic> cont
 }
 
 
+Future<int> numOfPosts() async {
+    Uri url = sharedPrefs.getUrl("/posts");
+    final response = await http.get(url, headers: {'Content-Type': 'application/json'});
+  if (response.statusCode == 200) {
+    final List<dynamic> posts = json.decode(response.body);
+    return posts.length;
+  } else {
+    throw Exception('Error cargando posts en funcion numOfPosts');
+  }
+}
+
+
+
+Future<void> addImagesToPost(int postId, Map<String, dynamic> images) async {
+  Uri url = sharedPrefs.getUrl("/posts/$postId/images");
+  images["post_id"] = postId;
+  final response = await http.post(
+    url,
+    headers: {'Content-Type': 'application/json'},
+    body: json.encode(images),
+  );
+  if (response.statusCode == 200) {
+    print('Se añadieron imagenes a post $postId');
+  } else {
+    throw Exception('Error añadiendo imagenes a post $postId ${response.body}');
+  }
+}
+
+
+Future<List<dynamic>> fetchImage(int postId) async{
+    try {
+    Uri url = sharedPrefs.getUrl("/post/$postId/images");
+    final response = await http.get(url, headers: {'ngrok-skip-browser-warning': 'true'});
+    print(response.body);
+    if (response.statusCode == 200) {
+      print("imagenes de post $postId cargadas");
+      return json.decode(response.body);
+    } else {
+      print('Error al cargar imagenes. status code: ${response.statusCode}');
+      Fluttertoast.showToast(
+        msg: 'Error al cargar los imagenes. status code: ${response.statusCode}',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: const Color.fromARGB(255, 162, 28, 28),
+        textColor: Colors.white,
+      );
+      return [];
+    }
+  } catch (e) {  
+    print('Error al cargar imagenes: $e');
+    return [];
+  }
+}
 
 
 Future<List<dynamic>> fetchPosts(String link) async {
   try {
-    var url = Uri.https(link, "/posts");
+    Uri url = sharedPrefs.getUrl("/posts");
+    
     final response = await http.get(url, headers: {'ngrok-skip-browser-warning': 'true'});
     print(response.body);
     if (response.statusCode == 200) {
@@ -57,8 +112,9 @@ Future<List<dynamic>> fetchPosts(String link) async {
   }
 }
 
-  Future<void> addCommentToPost(String link, int postId, Map<String, dynamic> commentData) async {
-  var url = Uri.https(link, "/posts/$postId");
+Future<void> addCommentToPost(String link, int postId, Map<String, dynamic> commentData) async {
+  Uri url = sharedPrefs.getUrl("/posts/$postId");
+  
   final response = await http.put(
     url,
     headers: {'Content-Type': 'application/json'},
@@ -89,11 +145,16 @@ Future<List<dynamic>> fetchPosts(String link) async {
 
 Future<Map<String, dynamic>> fetchPost(String link, postId) async {
   try {
-    var url = Uri.https(link, "/posts/$postId");
+    Uri url = sharedPrefs.getUrl("/posts/$postId");
+    Uri imageUrl = sharedPrefs.getUrl("/posts/$postId/images");
     final response = await http.get(url, headers: {'ngrok-skip-browser-warning': 'true'});
-    
+    final imageResponse = await http.get(imageUrl, headers: {'ngrok-skip-browser-warning': 'true'});
     if (response.statusCode == 200) {
-      return json.decode(response.body);
+      Map<String, dynamic> postData = {
+        "post": json.decode(response.body),
+        "images": json.decode(imageResponse.body),
+      };
+      return postData;
     } else {
       print('Error al cargar los posts. Status Code: ${response.statusCode}');
 
@@ -107,9 +168,9 @@ Future<Map<String, dynamic>> fetchPost(String link, postId) async {
 }
 
 
-   Future<List<dynamic>> fetchUsers(String link) async {
+  Future<List<dynamic>> fetchUsers(String link) async {
     try {
-      var url = Uri.https(link, "/userlist");
+      Uri url = sharedPrefs.getUrl("/userlist");
       final response = await http.get(url, headers: {'ngrok-skip-browser-warning': 'true'});
       if (response.statusCode == 200) {
           print('Userlist cargada');
@@ -123,6 +184,7 @@ Future<Map<String, dynamic>> fetchPost(String link, postId) async {
         backgroundColor: Colors.red,
         textColor: Colors.white,
       );
+        print(url);
         print('Error al cargar userlist: ${response.statusCode}');
         return [];
       }
@@ -142,7 +204,7 @@ Future<Map<String, dynamic>> fetchPost(String link, postId) async {
 
 Future<String> addUser(String link, Map<String, dynamic> userMap) async {
   try {
-    var url = Uri.https(link, "/userlist");  
+    Uri url = sharedPrefs.getUrl("/userlist"); 
     var response = await http.post(url, headers: {'ngrok-skip-browser-warning': 'true', 'Content-Type': 'application/json'}, body: jsonEncode(userMap));
     if (response.statusCode == 200) {
       return "Usuario añadido";
@@ -165,5 +227,42 @@ Future<String> addUser(String link, Map<String, dynamic> userMap) async {
         textColor: Colors.white,
       );
     return "Error añadiendo usuario: $e";
+  }
+}
+
+
+Future<String> addPost(Map<String, dynamic> postMap) async {
+  try {
+    Uri url = sharedPrefs.getUrl("/posts");
+    var response = await http.post(url, headers: {'ngrok-skip-browser-warning': 'true', 'Content-Type': 'application/json'}, body: jsonEncode(postMap));
+    if (response.statusCode == 200) {
+      Fluttertoast.showToast(
+        msg: "Wakala reportado con éxito",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+      );
+      
+      return "Wakala reportado con éxito";
+    } else {
+      Fluttertoast.showToast(
+        msg: "Error al denunciar. Status code: ${response.statusCode}",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return "Error al denunciar Wakala: ${response.statusCode}";
+    }
+  } catch (e) {
+      Fluttertoast.showToast(
+        msg: "Error : $e",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+    return "Error: $e";
   }
 }
