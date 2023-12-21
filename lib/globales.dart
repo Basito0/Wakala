013bfe8b919e3528cc6f0075704582e6928c59a5
orbb4 <114522263+orbb4@users.noreply.github.com>
2023-12-21 00:17:00 -0300

    se guarda foto para ver una foto mas grande en postDetails

--- lib/globales.dart
+++ lib/globales.dart
@@ -1,5 +1,6 @@
 import 'package:image_picker/image_picker.dart';
 
+import 'dart:typed_data';
 String sectorTempText="";
 String descripcionTempText="";
 
@@ -9,3 +10,4 @@ String usuarioActual = "none";
 int current_post_id = 0;
 XFile? foto1;
 XFile? foto2;
+Uint8List? fotoParaPost_Details;
